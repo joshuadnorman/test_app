e4ee2cdc1f906267dcbd867f308ec57a601a7ebd
Joshua Norman <joshuadnorman@gmail.com>
2022-05-09 18:11:39 +0000

    add methods and partials to make code reusable

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show,:edit,:update,:destroy]
+    
     def show
         @article = Article.find(params[:id])
     end
@@ -12,7 +14,7 @@ class ArticlesController < ApplicationController
     end 
     
     def create
-        @article = Article.new(params.require(:article).permit(:title,:description))
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article saved successfully"
         redirect_to @article
@@ -22,15 +24,33 @@ class ArticlesController < ApplicationController
     end 
     
     def edit
-        @article = Article.find(params[:id])
     end 
     
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title,:description))
-            flash[:notice] = "Article successfully created"
+        if @article.update(article_params)
+            flash[:notice] = "Article successfully updated"
+            render 'show'
         else 
             render 'edit'
         end 
+    end 
+    
+    def destroy
+        if (@article.destroy)
+            flash[:notice] = "Article #{params[:id]} has been deleted"
+            # render 'show'
+            redirect_to articles_path
+        else 
+            flash[:notice] = "Unable to delete article"
+        end 
+    end 
+    
+    private
+    def set_article
+        @article = Article.find(params[:id])
+    end 
+    
+    def article_params
+        params.require(:article).permit(:title,:description)
     end 
 end 
