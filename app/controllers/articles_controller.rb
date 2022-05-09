c54e11257ad320d36c916208c8168c90962c2a42
Joshua Norman <joshuadnorman@gmail.com>
2022-05-09 16:41:36 +0000

    Create and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,36 @@
 class ArticlesController < ApplicationController
     def show
-        debugger
         @article = Article.find(params[:id])
     end
     
     def index
         @articles = Article.all
+    end 
+    
+    def new 
+        @article = Article.new
+    end 
+    
+    def create
+        @article = Article.new(params.require(:article).permit(:title,:description))
+        if @article.save
+            flash[:notice] = "Article saved successfully"
+        redirect_to @article
+        else 
+            render 'new'
+        end 
+    end 
+    
+    def edit
+        @article = Article.find(params[:id])
+    end 
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title,:description))
+            flash[:notice] = "Article successfully created"
+        else 
+            render 'edit'
+        end 
     end 
 end 
