3e72d584304f72bb81c8402f3bec5d12554a63ef
Joshua Norman <joshuadnorman@gmail.com>
2022-06-06 17:47:14 +0000

    TDD for categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,29 @@
+class CategoriesController<ApplicationController
+    
+    def show
+        @category = Category.find(params[:id])
+    end 
+    
+    def index
+    end 
+    
+    def new 
+        @category = Category.new
+    end 
+    
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "New Category created successfully"
+            redirect_to @category
+        else
+            render 'new'
+        end 
+    end 
+    
+    private
+    
+    def category_params
+        params.require(:category).permit(:name)
+    end 
+end 
